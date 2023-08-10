4ae788d87fd3f114d2a750452b37d8fb584bbb0d
Villzies <gemberling.stephan@att.net>
2023-08-09 21:15:52 -0500

    Created arbitrary questions

---
+++ script.js
@@ -0,0 +1,15 @@
+const quizquestions = [
+    { question: "Do you like pancakes?",
+      answers: {
+        a: 'Yes',
+        b: 'No',
+      }
+    },
+
+    { question: "Do you like waffles?",
+      answers: {
+        a: 'Yes',
+        b: 'No',
+      }
+}
+];
