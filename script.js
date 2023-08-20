b697658a8717fe05e2cbede31b03a3599b72bed4
Villzies <gemberling.stephan@att.net>
2023-08-20 17:13:57 -0500

    added js for basic quiz functionality

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+const quizcontainer =  document.getElementById('quizcontainer');
+const startbutton = document.getElementById('start');
+const beginquiz = document.getElementById('quizlist');
+const next = document.getElementById('next');
+const question = document.getElementById('question');
+const choice1 = document.getElementById('choice1');
+const choice2 = document.getElementById('choice2');
+let questionindex = 0;
+
 const quizquestions = [
-    { question: "Do you like pancakes?",
+    {question: "Do you like pancakes?",
       answers: {
         a: 'Yes',
         b: 'No',
       }
     },
 
-    { question: "Do you like waffles?",
+    {question: "Do you like waffles?",
       answers: {
         a: 'Yes',
         b: 'No',
       }
 }
 ];
+
+const displayquiz = function(){
+question.innerHTML = quizquestions[questionindex].question;
+choice1.innerHTML = quizquestions[questionindex].answers.a;
+choice2.innerHTML = quizquestions[questionindex].answers.b;
+};
+
+startbutton.addEventListener("click", function() {
+ beginquiz.style.display="block";
+ next.style.display="block";
+ startbutton.style.display="none";
+ displayquiz();
+ console.log("hello world");
+});
+
+next.addEventListener("click", function(){
+questionindex++;
+displayquiz();
+});
